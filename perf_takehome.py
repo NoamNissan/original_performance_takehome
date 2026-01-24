3f08a53f5145f739dc41dfe40ddafda4dd9598ef
Noam Nissan <non847@gmail.com>
2026-01-24 18:14:22 +0200

    save progress factor 31.167510548523207

--- perf_takehome.py
+++ perf_takehome.py
@@ -291,7 +291,7 @@ class KernelBuilder:
 
         body = []  # array of slots
         round_num = -1
-        for round in range(rounds):
+        for round in range(forest_height):
             for i in range(0, batch_size, VLEN):
                 vbatch = int(i/VLEN)
                 round_num += 1
@@ -329,9 +329,96 @@ class KernelBuilder:
                 
         # now combine everything
         # body_instrs = self.build_multi(body)
-        body_instrs = self.build_compress(body, batch_size, rounds)
+        body_instrs = self.build_compress(body, batch_size, forest_height)
         self.instrs.extend(body_instrs)
 
+        body = []  # array of slots
+        round_num = -1
+        for round in range(1):
+            for i in range(0, batch_size, VLEN):
+                vbatch = int(i/VLEN)
+                round_num += 1
+                # mb_num = vbatch % self.mb_size
+                mb_num = round_num % self.mb_size
+
+                vtmp1 = arr_vtmp1[mb_num]
+                vtmp2 = arr_vtmp2[mb_num]
+
+                tmp_idx_v = mega_idx_v[vbatch]
+                tmp_val_v = mega_val_v[vbatch]
+
+                tmp_node_val_v = arr_tmp_node_val_v[mb_num]
+                tmp_addr_v = arr_tmp_addr_v[mb_num]
+
+                body.append(("valu", ("+", tmp_addr_v, tmp_idx_v, vforest_values_p)))
+                for j in range(VLEN):
+                    # node_val = mem[forest_values_p + idx]
+                    body.append(("load", ("load_offset", tmp_node_val_v, tmp_addr_v, j)))
+
+                # val = myhash(val ^ node_val)
+                body.append(("valu", ("^", tmp_val_v, tmp_val_v, tmp_node_val_v)))
+                body.extend(self.build_vhash(tmp_val_v, vtmp1, vtmp2, round, i))
+                self.add("debug", ("comment", "Vhash finished"))
+ 
+                # all goes to zero               
+                body.append(("valu", ("vbroadcast", tmp_idx_v, zero_const)))
+
+                # # idx = 2*idx + (1 if val % 2 == 0 else 2)
+                # body.append(("valu", ("%", vtmp1, tmp_val_v, vtwo)))
+                # body.append(("valu", ("+", vtmp1, vtmp1, vone)))
+                # body.append(("valu", ("multiply_add", tmp_idx_v, tmp_idx_v, vtwo, vtmp1)))
+
+                # # idx = 0 if idx >= n_nodes else idx
+                # body.append(("valu", ("<", vtmp1, tmp_idx_v, vn_nodes)))
+                # body.append(("flow", ("vselect", tmp_idx_v, vtmp1, tmp_idx_v, vzero)))
+                
+        # now combine everything
+        # body_instrs = self.build_multi(body)
+        body_instrs = self.build_compress(body, batch_size, 1)
+        self.instrs.extend(body_instrs)
+
+        body = []  # array of slots
+        round_num = -1
+        for round in range(rounds-forest_height-1):
+            for i in range(0, batch_size, VLEN):
+                vbatch = int(i/VLEN)
+                round_num += 1
+                # mb_num = vbatch % self.mb_size
+                mb_num = round_num % self.mb_size
+
+                vtmp1 = arr_vtmp1[mb_num]
+                vtmp2 = arr_vtmp2[mb_num]
+
+                tmp_idx_v = mega_idx_v[vbatch]
+                tmp_val_v = mega_val_v[vbatch]
+
+                tmp_node_val_v = arr_tmp_node_val_v[mb_num]
+                tmp_addr_v = arr_tmp_addr_v[mb_num]
+
+                body.append(("valu", ("+", tmp_addr_v, tmp_idx_v, vforest_values_p)))
+                for j in range(VLEN):
+                    # node_val = mem[forest_values_p + idx]
+                    body.append(("load", ("load_offset", tmp_node_val_v, tmp_addr_v, j)))
+
+                # val = myhash(val ^ node_val)
+                body.append(("valu", ("^", tmp_val_v, tmp_val_v, tmp_node_val_v)))
+                body.extend(self.build_vhash(tmp_val_v, vtmp1, vtmp2, round, i))
+                self.add("debug", ("comment", "Vhash finished"))
+                
+
+                # idx = 2*idx + (1 if val % 2 == 0 else 2)
+                body.append(("valu", ("%", vtmp1, tmp_val_v, vtwo)))
+                body.append(("valu", ("+", vtmp1, vtmp1, vone)))
+                body.append(("valu", ("multiply_add", tmp_idx_v, tmp_idx_v, vtwo, vtmp1)))
+
+                # idx = 0 if idx >= n_nodes else idx
+                body.append(("valu", ("<", vtmp1, tmp_idx_v, vn_nodes)))
+                body.append(("flow", ("vselect", tmp_idx_v, vtmp1, tmp_idx_v, vzero)))
+                
+        # now combine everything
+        # body_instrs = self.build_multi(body)
+        body_instrs = self.build_compress(body, batch_size, rounds - forest_height - 1)
+        self.instrs.extend(body_instrs)
 
         body = []
         for i in range(0, batch_size, VLEN):
@@ -459,7 +546,7 @@ class Tests(unittest.TestCase):
     #             )
 
     def test_kernel_cycles(self):
-        # do_kernel_test(1, 2, 64, trace=True, prints=True)
+        # do_kernel_test(4, 6, 8, trace=True, prints=True)
         # do_kernel_test(1, 16, 240, trace=False, prints=False)
         do_kernel_test(10, 16, 256, trace=False, prints=False)
 
